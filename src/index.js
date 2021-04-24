36ffbd831f332c37fb7df14c3dda69c00b14d141
Pratyush Garg <pratyushhg@gmail.com>
2021-04-24 18:32:49 +0530

    languages added + suspense

--- src/index.js
+++ src/index.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import "bootstrap/dist/js/bootstrap.js";
+
+import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.js";
+import "flag-icon-css/css/flag-icon.min.css";
+
 i18next
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
   .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    supportedLngs: ["en", "fr", "ar"],
+    supportedLngs: ["en", "hi", "fr"],
     fallbackLng: "en",
+    debug: false,
+    // Options for language detector
     detection: {
-      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      order: ["path", "cookie", "htmlTag"],
       caches: ["cookie"],
     },
-
+    // react: { useSuspense: false },
     backend: {
       loadPath: "/assets/locales/{{lng}}/translation.json",
     },
-    react: {
-      useSuspense: false,
-    },
   });
 
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h3>Loading..</h3>
+  </div>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById("root")
 );
